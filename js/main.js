e55f956d4a3b128b566a0ba6c54bd554707649b7
David Gómez Ramírez <forest@MacBook-Air-de-David.local>
2025-10-27 09:52:25 -0500

    Correción de error en móviles para que el menú del header sea visible de la mejor forma.

--- js/main.js
+++ js/main.js
@@ -14,3 +14,17 @@ document.addEventListener("DOMContentLoaded", () => {
 
   sections.forEach(section => observer.observe(section))
 });
+
+const navToggle = document.querySelector('.nav-toggle');
+const navClose = document.querySelector('.nav-close button');
+const navLinks = document.querySelector('.nav-links');
+const navOverlay = document.querySelector('.nav-overlay');
+
+
+function toggleMenu() {
+  navLinks.classList.toggle('show');
+  navOverlay.classList.toggle('show');
+}
+navToggle.addEventListener('click', toggleMenu);
+navClose.addEventListener('click', toggleMenu);
+navOverlay.addEventListener('click', toggleMenu);
